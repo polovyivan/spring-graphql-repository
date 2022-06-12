59f17f2bf12db3c56fef0fd8f7c2221844ee9a13
Ivan Polovyi <ivan.polovyi@sensedia.com>
2022-06-11 22:02:47 -0300

    removed unused field

--- src/main/java/com/polovyi/ivan/projection/PurchaseTransactionProjection.java
+++ src/main/java/com/polovyi/ivan/projection/PurchaseTransactionProjection.java
@@ -1,13 +1,10 @@
 package com.polovyi.ivan.projection;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public interface PurchaseTransactionProjection {
 
     String getId();
-
-    BigDecimal getAmount();
 
     String getPaymentType();
 
